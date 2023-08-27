7b72591bee07ae6d345618969393102bee4a45b7
Qkrwnsgus0522 <134922079+Qkrwnsgus0522@users.noreply.github.com>
2023-08-27 17:56:38 +0900

    Add files via upload

---
+++ day49_t/src/main/java/controller/TestServlet.java
@@ -0,0 +1,35 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test.do")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public TestServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ajax 로그 : " + request.getParameter("res"));
+		PrintWriter out = response.getWriter();
+		if (request.getParameter("res").equals("1")) {
+			out.print("1");			
+		}
+		else {
+			out.print("2");
+		}
+	}
+
+}
